840402e3106f1de7d78408bdb5bd66b0dc1bce6b
Tiantian Ye <tiantian@tiantians-mba.home>
2017-12-26 23:20:35 -0800

    rename file

--- mdsite/commands/buildsite.py
+++ mdsite/commands/buildsite.py
@@ -1,10 +1,11 @@
 import os
-from mdsite.utils import postparse, homebuilder
+from mdsite.utils import postparser, homebuilder
 
 config = {
     'site_url': None,  # if has site_url, use absolute url
     'site_dir': 'site',
-    'permalink': ':year:/:month:/:day:/:title:.html'
+    'permalink': ':year:/:month:/:day:/:title:.html',
+    'template': None
 }
 
 
@@ -17,11 +18,10 @@ def build_site():
     # get post list
     # ordered by date, start from latest
     # record the current path depth, start with . (site_dir)
-    path_depth = postparse.PathDepth()
-    posts = postparse.get_post_list(post_dir, path_depth, config)
+    path_depth = postparser.PathDepth()
+    posts = postparser.get_post_list(post_dir, path_depth, config)
     # for post in posts:
     #     print post.title, post.date, post.url
     #     path_depth.set_base_path(post.html_path)
     if posts is not None:
         homebuilder.home_builder(posts, config)
-
